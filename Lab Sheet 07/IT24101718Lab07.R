9d411404f06611fe15169cfa4e167bd767b89dd5
IT24101718 <it24101718@my.sliit.lk>
2025-09-16 13:22:01 +0530

    Add files via upload

---
+++ Lab Sheet 07/IT24101718Lab07.R
@@ -0,0 +1,13 @@
+setwd("C:\\Users\\sadew\\Downloads\\IT24101718Lab07")
+
+# E1
+punif(25, min=0, max=40) - punif(10, min=0, max=40)
+
+# E2
+pexp(2, rate = 1/3)
+
+# E3
+#i)
+1 - pnorm(130, mean = 100, sd = 15)
+#ii)
+qnorm(0.95, mean = 100, sd = 15)
